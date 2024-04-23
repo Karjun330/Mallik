fb9c56c4ca2e259dd23a10af9dc6b477856fafdb
Karjun330 <73626888+Karjun330@users.noreply.github.com>
2024-04-23 14:16:35 -0400

    Modified Code

--- src/test/java/com/bhe/test/api/DemoApplicationTests.java
+++ src/test/java/com/bhe/test/api/DemoApplicationTests.java
@@ -5,36 +5,36 @@ import java.net.MalformedURLException;
 
 
 public class DemoApplicationTests extends ApplicationTests {
+//
+//       // @Test
+//        public void apiCalls() {
+//            // openWorkspace();
+//            System.out.println("Launching first API call");
+//            //First API Call
+//            postOAuth();
+//            System.out.println("Launching Second API call");
+//            //Second API Call
+//            locateCopy();
+//        }
 
-       // @Test
-        public void apiCalls() {
-            // openWorkspace();
-            System.out.println("Launching first API call");
-            //First API Call
-            postOAuth();
-            System.out.println("Launching Second API call");
-            //Second API Call
-            locateCopy();
-        }
-
-    //    @Test
-       public void salesforceWeb() throws InterruptedException {
-            login();
-            verifyServiceAppointStatus();
-            dispatchServiceAppointment();
-        }
+//    //    @Test
+//       public void salesforceWeb() throws InterruptedException {
+//            login();
+//            verifyServiceAppointStatus();
+//            dispatchServiceAppointment();
+//        }
 
         @Test
        public void simulatorAppiumTest() throws MalformedURLException {
             simulatorTest();
         }
 
-       // @Test
-        public void VerifyCompletedJob()
-        {
-            login();
-           // CompletedServiceJob();
-      }
+//       // @Test
+//        public void VerifyCompletedJob()
+//        {
+//            login();
+//           // CompletedServiceJob();
+//      }
 
 
     }
